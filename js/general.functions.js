7c9c3e0bf614d15b2b0b8d3e4118b0f99a48f36b
Benjamin Arnedo <budaloco@gmail.com>
2012-09-04 10:45:27 -0300

    Cambios en el Javascript general
    Se deshabilitaron las transiciones de todas las páginas. Se devolvió el
    data-position fixed a las cabeceras y se removió los "arreglos" de CSS
    ya que no arreglaron nada.

--- js/general.functions.js
+++ js/general.functions.js
@@ -2,6 +2,14 @@
 	jQuery Mobile Boilerplate
 	application.js
 */
+
+$(document).bind("mobileinit", function(){
+	$.mobile.defaultPageTransition = 'none';
+	$.mobile.defaultDialogTransition = 'none';
+	$.mobile.useFastClick = true;
+});
+	
+
 $(document).bind("pageinit", function(event){
 	//Adjust scroller in fullscreen mode
 	if (window.navigator.standalone) {
